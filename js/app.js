5c661b25e4c583a2c14c5f4969c07a5b56280755
hackij <hackij@users.noreply.github.com>
2026-03-25 11:11:25 +0100

    Refine exercise details card

--- js/app.js
+++ js/app.js
@@ -635,7 +635,7 @@ function getStepContent(stepId) {
           <dl class="exercise-facts">
             <div><dt>Standort</dt><dd>Bayreuth</dd></div>
             <div><dt>Gebäudelänge</dt><dd>10 m</dd></div>
-            <div><dt>Gebäudebreite</dt><dd>10 m</dd></div>
+            <div><dt>Gebäudebreite</dt><dd>8 m</dd></div>
             <div><dt>Gebäudehöhe</dt><dd>9 m</dd></div>
             <div><dt>Dachneigung</dt><dd>20°</dd></div>
             <div><dt>Scharbreite</dt><dd>590 mm</dd></div>
